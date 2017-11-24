58bd7b8144f5bb3ddcba207ba4a56f6b206a3efc
Unknown <louis.benjamin@live.fr>
2017-11-24 17:34:49 +0100

    start of game screen

---
+++ app/src/main/java/com/quidditch/benjaminlouis/quidditch_live_ref/Game.java
@@ -0,0 +1,13 @@
+package com.quidditch.benjaminlouis.quidditch_live_ref;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Game extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+    }
+}
